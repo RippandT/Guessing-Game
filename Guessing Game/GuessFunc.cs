059e2c30b8a7fbe07d371314dbe20e0de928f209
RippandT <56902025+RippandT@users.noreply.github.com>
2020-07-22 21:37:03 +0800

    Add files via upload

---
+++ Guessing Game/GuessFunc.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace GuessingNumbers
+{
+    public class GuessFunc
+    {
+        /* Variables */
+        public int highNum;
+        public int randomNum;
+        public int tries;
+        public int diffiSetting;
+        public int attempt;
+        public int loop = 0;
+        public bool win = false;
+        public bool gameRun = true;
+        public bool diffiChoice = true;
+        
+        public void endGame() //Endgame result and play again prompt
+        {
+            Console.Clear();
+
+            if (win)
+            {
+                Console.WriteLine("CONGRATULATIONS!");
+                Console.WriteLine("YOU GOT THE NUMBER! The number was {0}!\n", randomNum);
+            }
+            else
+            {
+                Console.WriteLine("YOU LOST!");
+                Console.WriteLine("The number was {0}. Better luck next time\n", randomNum);
+            }
+
+            Console.WriteLine("Do you want to play again? [Y/N]");
+            bool exit = true;
+            while (exit)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Y:
+                        Console.Clear();
+                        loop = 0;
+                        diffiChoice = true;
+                        win = false;
+                        exit = false;
+                        break;
+                    case ConsoleKey.N:
+                        Console.Clear();
+                        Console.WriteLine("THANK YOU FOR PLAYING MY GAME!");
+                        Console.WriteLine("..Press any key to exit...");
+                        gameRun = false;
+                        exit = false;
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
+            }
+        }
+        public void checker(int guess) //Number checker
+        {
+            if (guess < randomNum)
+            {
+                Console.WriteLine("The number is too low, guess higher");
+            }
+            else if (guess > randomNum)
+            {
+                Console.WriteLine("The number is too high, guess lower");
+            }
+            else
+            {
+                win = true;
+            }
+
+            attempt = guess; //Return
+        }
+        public void difficulty() //Difficulty selection
+        {
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.E:
+                    highNum = 11;
+                    tries = 5;
+                    diffiSetting = 0;
+                    diffiChoice = false;
+                    break;
+                case ConsoleKey.M:
+                    highNum = 16;
+                    tries = 4;
+                    diffiSetting = 1;
+                    diffiChoice = false;
+                    break;
+                case ConsoleKey.H:
+                    highNum = 21;
+                    tries = 3;
+                    diffiSetting = 2;
+                    diffiChoice = false;
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("===== Guess the Number =====\n");
+                    Console.WriteLine("Wrong input, try again\n");
+                    break;
+            }
+        }
+    }
+}
---
+++ Guessing Game/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace GuessingNumbers
+{
+    public enum Settings
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Class Calls */
+            GuessFunc funct = new GuessFunc();
+            Random rng = new Random();
+
+            /* Main Program */
+            while (funct.gameRun)
+            {
+                /* Difficulty Selection */
+                Console.WriteLine("===== Guess the Number =====\n");
+                while (funct.diffiChoice)
+                {
+                    Console.WriteLine("Select difficulty: (Enter the corresponding letter)\n");
+                    Console.WriteLine("[E]asy - 5 tries. Numbers 1-10");
+                    Console.WriteLine("[M]edium - 4 tries. Numbers 1-15");
+                    Console.WriteLine("[H]ard - 3 tries. Numbers 1-20");
+
+                    funct.difficulty(); // the prompt
+                }
+
+                funct.randomNum = rng.Next(1, funct.highNum); // Set the random number
+                Console.Clear(); //Clears screen before the game starts
+
+                /* The Game */
+                while (funct.loop < funct.tries)
+                {
+                    Console.Write(Enum.GetName(typeof(Settings), funct.diffiSetting));
+                    Console.WriteLine(". {0} / {1} tries", funct.loop + 1, funct.tries);
+                    Console.Write("Guess the number: ");
+                    int guess;
+                    if (int.TryParse(Console.ReadLine(), out guess) && guess < funct.highNum)
+                    {
+                        Console.Clear();
+                        funct.checker(guess);
+                        if (funct.win) { break; }
+                        Console.WriteLine("Last guess: {0}\n", funct.attempt);
+                        funct.loop++;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Wrong input");
+                        Console.WriteLine("Last guess: {0}\n", funct.attempt);
+                    }
+                }
+
+                funct.endGame();
+            }
+        }
+    }
+}
